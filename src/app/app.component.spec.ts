d548ad43b113ff39c4ee21bbadc0efcab1bf7ea2
Jejomar Batitis <jcbatitis@hotmail.com>
2025-01-21 22:16:41 +1100

    MAIN: added prettier and eslint config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Pomodoro');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, Pomodoro'
+    );
   });
 });
